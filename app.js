8c966db51c9906ed85c7a60700969f758ad4021c
Les Carbonaro <lcarbonaro@gmail.com>
2020-09-08 08:52:32 -0400

    add cors

--- app.js
+++ app.js
@@ -8,16 +8,20 @@ var api = require('./api');
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+const cors = require('cors');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
 
+/*
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+*/
+app.use(cors());
 
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
